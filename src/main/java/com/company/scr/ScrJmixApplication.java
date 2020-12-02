e77b78efcd65db6c2112595ccacd7ed68797d98e
Vlad Minaev <minaev@haulmont.com>
2020-12-02 14:05:14 +0400

    Initial

---
+++ src/main/java/com/company/scr/ScrJmixApplication.java
@@ -0,0 +1,25 @@
+package com.company.scr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class ScrJmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrJmixApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix="main.datasource")
+	DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+}
---
+++ src/test/java/com/company/scr/ScrJmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.scr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrJmixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
