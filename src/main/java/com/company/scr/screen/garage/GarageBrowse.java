222fdb01fbbb6a0a141e280a68ef590edc8e78f1
Vadim Basko <vadim.p.basko@gmail.com>
2021-01-15 11:38:08 +0400

    garage screens

---
+++ src/main/java/com/company/scr/screen/garage/GarageBrowse.java
@@ -0,0 +1,10 @@
+package com.company.scr.screen.garage;
+
+import io.jmix.ui.screen.*;
+import com.company.scr.entity.Garage;
+
+@UiController("scr$Garage.browse")
+@UiDescriptor("garage-browse.xml")
+@LookupComponent("garagesTable")
+public class GarageBrowse extends StandardLookup<Garage> {
+}
---
+++ src/main/java/com/company/scr/screen/garage/GarageEdit.java
@@ -0,0 +1,10 @@
+package com.company.scr.screen.garage;
+
+import io.jmix.ui.screen.*;
+import com.company.scr.entity.Garage;
+
+@UiController("scr$Garage.edit")
+@UiDescriptor("garage-edit.xml")
+@EditedEntityContainer("garageDc")
+public class GarageEdit extends StandardEditor<Garage> {
+}
