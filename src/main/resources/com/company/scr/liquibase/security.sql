5d5d94a35873eb3e37771c8ce782977223802129
Vadim Basko <vadim.p.basko@gmail.com>
2021-02-11 15:21:34 +0400

    init data - users

---
+++ src/main/resources/com/company/scr/liquibase/security.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.scr_user (id, version, username, password, first_name, last_name, email, enabled, phone)
+VALUES
+('af89f9b9-5e64-bdf9-2466-5da9c91cf3d4', 1, 'mechanic', '{bcrypt}$2a$10$9beORT6c61zvR1fbpcaqd.pUnIIKK0EKGT3IbssNpuHxK5WJq1VYe', 'John', 'Doe', 'jd@example.com', true, NULL),
+('62864fc8-0273-7c57-890d-314c1fd2fde3', 1, 'manager', '{bcrypt}$2a$10$pO.Edb8qWb3VTP0RSjRXXefIzLAVpXLvhZobCdpCYODx5FP8.4qWG', 'Ivan', 'Petrov', 'ivanp@example.com', true, NULL);
