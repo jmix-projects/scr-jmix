48b2b8b418f493170aa2ce3db67eccdaeb6c62c7
Vadim Basko <vadim.p.basko@gmail.com>
2021-03-16 10:36:31 +0400

    Add test entity with custom id name and type

---
+++ src/main/java/com/company/scr/entity/test/ids/TrickyIdTestEntity.java
@@ -0,0 +1,37 @@
+package com.company.scr.entity.test.ids;
+
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@JmixEntity
+@Table(name = "SCR_TRICKY_ID_TEST_ENTITY")
+@Entity(name = "scr_TrickyIdTestEntity")
+public class TrickyIdTestEntity {
+
+    @Column(name = "TRICKY_ID", nullable = false)
+    @Id
+    private Long trickyId;
+
+    @Column(name = "OTHER_ATTR")
+    private String otherAttr;
+
+    public String getOtherAttr() {
+        return otherAttr;
+    }
+
+    public void setOtherAttr(String otherAttr) {
+        this.otherAttr = otherAttr;
+    }
+
+    public Long getTrickyId() {
+        return trickyId;
+    }
+
+    public void setTrickyId(Long trickyId) {
+        this.trickyId = trickyId;
+    }
+}
