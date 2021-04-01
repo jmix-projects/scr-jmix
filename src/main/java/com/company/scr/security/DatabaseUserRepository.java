5e921344427b37b47ef454b59cdbfe012c37c453
Vadim Basko <vadim.p.basko@gmail.com>
2021-04-01 21:16:57 +0400

    fix compilation error after jmix update

--- src/main/java/com/company/scr/security/DatabaseUserRepository.java
+++ src/main/java/com/company/scr/security/DatabaseUserRepository.java
@@ -3,7 +3,12 @@ package com.company.scr.security;
 import com.company.scr.entity.User;
 import io.jmix.securitydata.user.AbstractDatabaseUserRepository;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 @Primary
 @Component("scr_UserRepository")
@@ -12,5 +17,15 @@ public class DatabaseUserRepository extends AbstractDatabaseUserRepository<User>
     @Override
     protected Class<User> getUserClass() {
         return User.class;
+    }
+
+    @Override
+    public Map<UserDetails, String> resetPasswords(Set<UserDetails> users) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void resetRememberMe(Collection<UserDetails> users) {
+        throw new UnsupportedOperationException("Not implemented");
     }
 }
