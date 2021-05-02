36e70ae320c805f8b4db5d57f8daa5d233066ef2
Vlad Minaev <minaev@haulmont.com>
2021-05-02 17:52:37 +0400

    Add frontend module

---
+++ frontend/src/app/common/Centered.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import "./Centered.css";
+
+interface CenteredProps {
+  children?: ReactNode;
+}
+
+const Centered = ({ children }: CenteredProps) => {
+  return <div className="_centered">{children}</div>;
+};
+
+export default Centered;
