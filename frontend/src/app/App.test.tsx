56d622e0253ff1a453f45423c5ed12e065d9b895
Vlad Minaev <minaev@haulmont.com>
2021-05-08 15:54:38 +0400

    Frontend module added

---
+++ frontend/src/app/App.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { IntlProvider } from "react-intl";
+import { JmixAppProvider } from "@haulmont/jmix-react-core";
+import { initializeApp } from "@haulmont/jmix-rest";
+
+const jmixREST = initializeApp();
+
+const metadata = { entities: [], enums: [] };
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <JmixAppProvider jmixREST={jmixREST} metadata={metadata}>
+      <IntlProvider locale="en">
+        <App />
+      </IntlProvider>
+    </JmixAppProvider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ frontend/src/app/header/AppHeader.tsx
@@ -0,0 +1,43 @@
+import { LogoutOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
+import React, { useCallback } from "react";
+import { observer } from "mobx-react";
+import "./AppHeader.css";
+import { useMainStore } from "@haulmont/jmix-react-core";
+import { LanguageSwitcher } from "../../i18n/LanguageSwitcher";
+import { useIntl } from "react-intl";
+import JmixLightIcon from "../icons/JmixLightIcon";
+
+const AppHeader = observer(() => {
+  const intl = useIntl();
+  const mainStore = useMainStore();
+
+  const showLogoutConfirm = useCallback(() => {
+    Modal.confirm({
+      title: intl.formatMessage({ id: "header.logout.areYouSure" }),
+      okText: intl.formatMessage({ id: "header.logout.ok" }),
+      cancelText: intl.formatMessage({ id: "header.logout.cancel" }),
+      onOk: () => mainStore.logout()
+    });
+  }, [mainStore, intl]);
+
+  return (
+    <div className="app-header">
+      <JmixLightIcon className="app-header__icon" />
+
+      <div className="user-panel">
+        <LanguageSwitcher className="panelelement language-switcher -header" />
+        <span className="panelelement">{mainStore.userName}</span>
+        <Button
+          className="panelelement"
+          id="button_logout"
+          ghost={true}
+          icon={<LogoutOutlined />}
+          onClick={showLogoutConfirm}
+        />
+      </div>
+    </div>
+  );
+});
+
+export default AppHeader;
